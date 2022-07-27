e70e7e3193cc2965dfaf1e35761e522a2fb3ae16
Vinzce Yadao <yadaovinzce@gmail.com>
2022-07-27 16:43:03 -0400

    different projects

---
+++ Quiz game/main.py
@@ -0,0 +1,15 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for item in question_data:
+    question = Question(item["question"], item["correct_answer"])
+    question_bank.append(question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+quiz.end_game()
