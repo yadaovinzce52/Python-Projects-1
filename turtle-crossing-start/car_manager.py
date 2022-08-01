99eb893fc01c07a90f71515cd94badc4693ad8c1
Vinzce Yadao <yadaovinzce@gmail.com>
2022-07-31 20:43:10 -0400

    start of turtle crossing game

--- turtle-crossing-start/car_manager.py
+++ turtle-crossing-start/car_manager.py
@@ -1,7 +1,9 @@
+from turtle import Turtle
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+
--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -6,9 +6,18 @@ from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.title('Turtle crossing game')
 screen.tracer(0)
+
+player = Player()
+
+screen.listen()
+screen.onkey(key='w', fun=player.move)
+screen.onkey(key='Up', fun=player.move)
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+
