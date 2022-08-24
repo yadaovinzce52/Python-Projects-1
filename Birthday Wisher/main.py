22c3d4534d029ae45c6a4ecf6c1097b3aa40afd0
Vinzce Yadao <yadaovinzce@gmail.com>
2022-08-23 22:18:21 -0400

    Finished datetime

--- Birthday Wisher/main.py
+++ Birthday Wisher/main.py
@@ -1,14 +1,25 @@
 import smtplib
+import datetime as dt
+from random import choice
 
-my_email = 'Change this to sender email'
-password = 'Change this to app generated pw'
+now = dt.datetime.now()
+day_of_week = now.weekday()
+random_password = 'rjcahicpfwgluzsx'
 
-with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs='Change this to receiver email',
-        msg='Subject:Hello\n\nThis is the body of the email'
-    )
+if day_of_week == 0:
+    with open('quotes.txt', 'r') as quotes:
+        list_of_quotes = quotes.readlines()
+
+    random_quote = choice(list_of_quotes)
+    my_email = 'change to gmail'
+    password = random_password
+
+    with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs='change to yahoo',
+            msg=f'Subject:Motivational Quote!!\n\n{random_quote}'
+        )
 
